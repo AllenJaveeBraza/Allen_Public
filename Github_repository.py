067d037a6f8171155d3c6d3437b9a1c0ee59b231
AllenJaveeBraza <allenjaveebraza123@gmail.com>
2024-09-27 16:55:00 +0800

    Update Github_repository.py

--- Github_repository.py
+++ Github_repository.py
@@ -1,1 +1,19 @@
-print("Hello World")
+# Salary Amount
+Salary=input("Please input your monthly salary here:")
+if float(Salary)>=30000:
+    print("You are eligible for a loan")
+else:
+    print("You are not eligible for a loan.")
+    exit()
+
+# Loan Amount    
+Loan=input("Please insert loan amount.(Note that you may only loan for a certain amount depending on your monthly salary.):")
+if float(Loan)>=float(Salary)*10:
+    print("Loan amount is too high, please input a lower amount.")
+    exit()
+else:
+    Month=input("Loan eligible, how many months would you like to pay:")
+    print("You are about to pay",Loan, "in", Month , "Months.")
+    Total=float(Loan) + float(Loan) * 0.10
+    print(f"total payment with 10% Interest:{Total:.2f}")
+    print("Have a great day.")
