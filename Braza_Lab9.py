256f2e43dd60f27f9ff265bb513febc3d3e318ea
AllenJaveeBraza <allenjaveebraza123@gmail.com>
2024-11-04 22:52:55 +0800

    Update Braza_Lab9.py

--- Braza_Lab9.py
+++ Braza_Lab9.py
@@ -1,8 +1,8 @@
 Num = int(input("Enter the number of rows: "))
 
 i = 1
-for y in range(1, Num + 1):
-    for x in range(1, y + 1):
-        print(i,end=" ")
+for y in range(1, Num + 1): 
+    for x in range(1, y + 1): # for row == Num
+        print(i,end=" ") # avoid single line per row 
         i = i+1
     print()
